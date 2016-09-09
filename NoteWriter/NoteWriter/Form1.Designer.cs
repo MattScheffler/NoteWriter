7880958b6d37c7992cc8762254ae6371e12a6e0d
MattScheffler <mattscheffler4@gmail.com>
2016-09-09 16:06:52 -0500

    Added
    Moved from small-projects

---
+++ NoteWriter/NoteWriter/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace NoteWriter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonItalic = new System.Windows.Forms.Button();
+            this.buttonBold = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonMinimize = new System.Windows.Forms.Button();
+            this.buttonBoldItalic = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.labelFilename = new System.Windows.Forms.Label();
+            this.buttonUnderline = new System.Windows.Forms.Button();
+            this.buttonStrikeout = new System.Windows.Forms.Button();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox.Location = new System.Drawing.Point(45, 41);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(707, 629);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            // 
+            // buttonItalic
+            // 
+            this.buttonItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonItalic.Location = new System.Drawing.Point(45, 685);
+            this.buttonItalic.Name = "buttonItalic";
+            this.buttonItalic.Size = new System.Drawing.Size(76, 25);
+            this.buttonItalic.TabIndex = 1;
+            this.buttonItalic.Text = "Italic";
+            this.buttonItalic.UseVisualStyleBackColor = true;
+            this.buttonItalic.Click += new System.EventHandler(this.buttonItalic_Click);
+            // 
+            // buttonBold
+            // 
+            this.buttonBold.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBold.Location = new System.Drawing.Point(123, 685);
+            this.buttonBold.Name = "buttonBold";
+            this.buttonBold.Size = new System.Drawing.Size(76, 25);
+            this.buttonBold.TabIndex = 2;
+            this.buttonBold.Text = "Bold";
+            this.buttonBold.UseVisualStyleBackColor = true;
+            this.buttonBold.Click += new System.EventHandler(this.buttonBold_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "rtf";
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "Text files| *.txt|Rich text files|*.rtf";
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.Color.DeepPink;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(982, 24);
+            this.menuStrip.TabIndex = 3;
+            this.menuStrip.Text = "File";
+            this.menuStrip.MouseDown += new System.Windows.Forms.MouseEventHandler(this.menuStrip_MouseDown);
+            this.menuStrip.MouseMove += new System.Windows.Forms.MouseEventHandler(this.menuStrip_MouseMove);
+            this.menuStrip.MouseUp += new System.Windows.Forms.MouseEventHandler(this.menuStrip_MouseUp);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem1,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.newToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.newToolStripMenuItem.Text = "File :";
+            // 
+            // newToolStripMenuItem1
+            // 
+            this.newToolStripMenuItem1.BackColor = System.Drawing.Color.DeepPink;
+            this.newToolStripMenuItem1.Name = "newToolStripMenuItem1";
+            this.newToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.newToolStripMenuItem1.Text = "New";
+            this.newToolStripMenuItem1.Click += new System.EventHandler(this.newToolStripMenuItem1_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.BackColor = System.Drawing.Color.DeepPink;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.BackColor = System.Drawing.Color.DeepPink;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.BackColor = System.Drawing.Color.DeepPink;
+            this.exitToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.Color.DeepPink;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonClose.Location = new System.Drawing.Point(958, -2);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(26, 27);
+            this.buttonClose.TabIndex = 4;
+            this.buttonClose.Text = "X";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonMinimize
+            // 
+            this.buttonMinimize.BackColor = System.Drawing.Color.DeepPink;
+            this.buttonMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMinimize.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMinimize.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonMinimize.Location = new System.Drawing.Point(933, -2);
+            this.buttonMinimize.Name = "buttonMinimize";
+            this.buttonMinimize.Size = new System.Drawing.Size(26, 27);
+            this.buttonMinimize.TabIndex = 5;
+            this.buttonMinimize.Text = "-";
+            this.buttonMinimize.UseVisualStyleBackColor = false;
+            this.buttonMinimize.Click += new System.EventHandler(this.buttonMinimize_Click);
+            // 
+            // buttonBoldItalic
+            // 
+            this.buttonBoldItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBoldItalic.Location = new System.Drawing.Point(201, 685);
+            this.buttonBoldItalic.Name = "buttonBoldItalic";
+            this.buttonBoldItalic.Size = new System.Drawing.Size(76, 25);
+            this.buttonBoldItalic.TabIndex = 6;
+            this.buttonBoldItalic.Text = "Bold Italic";
+            this.buttonBoldItalic.UseVisualStyleBackColor = true;
+            this.buttonBoldItalic.Click += new System.EventHandler(this.buttonBoldItalic_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Text files|*.txt|Rich text file|*.rtf";
+            // 
+            // labelFilename
+            // 
+            this.labelFilename.AutoSize = true;
+            this.labelFilename.BackColor = System.Drawing.Color.DeepPink;
+            this.labelFilename.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFilename.Location = new System.Drawing.Point(48, 4);
+            this.labelFilename.Name = "labelFilename";
+            this.labelFilename.Size = new System.Drawing.Size(0, 15);
+            this.labelFilename.TabIndex = 7;
+            this.labelFilename.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonUnderline
+            // 
+            this.buttonUnderline.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUnderline.Location = new System.Drawing.Point(279, 685);
+            this.buttonUnderline.Name = "buttonUnderline";
+            this.buttonUnderline.Size = new System.Drawing.Size(76, 25);
+            this.buttonUnderline.TabIndex = 8;
+            this.buttonUnderline.Text = "Underline";
+            this.buttonUnderline.UseVisualStyleBackColor = true;
+            this.buttonUnderline.Click += new System.EventHandler(this.buttonUnderline_Click);
+            // 
+            // buttonStrikeout
+            // 
+            this.buttonStrikeout.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Strikeout, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStrikeout.Location = new System.Drawing.Point(358, 685);
+            this.buttonStrikeout.Name = "buttonStrikeout";
+            this.buttonStrikeout.Size = new System.Drawing.Size(76, 25);
+            this.buttonStrikeout.TabIndex = 9;
+            this.buttonStrikeout.Text = "Strikeout";
+            this.buttonStrikeout.UseVisualStyleBackColor = true;
+            this.buttonStrikeout.Click += new System.EventHandler(this.buttonStrikeout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::NoteWriter.Properties.Resources.NoteWriterPicture2;
+            this.ClientSize = new System.Drawing.Size(982, 734);
+            this.Controls.Add(this.buttonStrikeout);
+            this.Controls.Add(this.buttonUnderline);
+            this.Controls.Add(this.labelFilename);
+            this.Controls.Add(this.buttonBoldItalic);
+            this.Controls.Add(this.buttonMinimize);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonBold);
+            this.Controls.Add(this.buttonItalic);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.menuStrip);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button buttonItalic;
+        private System.Windows.Forms.Button buttonBold;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonMinimize;
+        private System.Windows.Forms.Button buttonBoldItalic;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Label labelFilename;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button buttonUnderline;
+        private System.Windows.Forms.Button buttonStrikeout;
+    }
+}
+
---
+++ NoteWriter/NoteWriter/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NoteWriter
+{
+    public partial class Form1 : Form
+    {
+        //Globals used in the window grag code
+        private bool mouseDown;
+        private Point location;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Start font change button events
+        private void buttonItalic_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.SelectionFont.Italic)
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Regular);
+            else
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Italic);
+            richTextBox.Focus();
+        }
+
+        private void buttonBold_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.SelectionFont.Bold)
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Regular);
+            else
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Bold);
+            richTextBox.Focus();
+        }
+
+        private void buttonBoldItalic_Click(object sender, EventArgs e)
+        {
+            if ((richTextBox.SelectionFont.Bold) && (richTextBox.SelectionFont.Italic))
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Regular);
+            else
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Bold | FontStyle.Italic);
+            richTextBox.Focus();
+        }
+
+        private void buttonUnderline_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.SelectionFont.Underline)
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Regular);
+            else
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Underline);
+            richTextBox.Focus();
+        }
+
+        private void buttonStrikeout_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.SelectionFont.Strikeout)
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Regular);
+            else
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Strikeout);
+            richTextBox.Focus();
+        }
+        //End font change button events
+
+        //Start top right buttons
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        //End top right buttons
+
+        //Start of window drag code, moves the window if blank space in the top menu is clicked down
+        private void menuStrip_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            location = e.Location;
+        }
+
+        private void menuStrip_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void menuStrip_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point((this.Location.X - location.X) + e.X, (this.Location.Y - location.Y) + e.Y);
+                this.Update();
+            }
+        }
+        //End of window drag code
+
+        //Start file menu buttons
+        private void newToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            labelFilename.Text = String.Empty;
+            richTextBox.Text = String.Empty;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog.FileName = String.Empty;
+            openFileDialog.InitialDirectory = System.IO.Path.GetDirectoryName(Application.StartupPath + '\\');
+            try
+            {
+                if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.Cancel)
+                    return;
+                else
+                {
+                    richTextBox.LoadFile(openFileDialog.FileName, RichTextBoxStreamType.RichText);
+                    int filenameStart = openFileDialog.FileName.LastIndexOf('\\');
+                    labelFilename.Text = openFileDialog.FileName.Substring(filenameStart + 1);
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.FileName = labelFilename.Text;
+            saveFileDialog.InitialDirectory = System.IO.Path.GetDirectoryName(Application.StartupPath + '\\');
+            try
+            {
+                if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.Cancel)
+                    return;
+                else
+                {
+                    richTextBox.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.RichText);
+                    int filenameStart = saveFileDialog.FileName.LastIndexOf('\\');
+                    labelFilename.Text = saveFileDialog.FileName.Substring(filenameStart + 1);
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //End file menu buttons
+    }
+}
